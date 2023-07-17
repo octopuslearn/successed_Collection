b7915750bbda2cd65a104226a014ef3cbb353974
octopuslearn <3148262242@qq.com>
2023-07-17 23:37:13 +0800

    整理好了硬件仿真 结论：串口0在硬件仿真时不能使用，否则报错

--- debugger_1.0/Debugger_test_1.0/src/main.cpp
+++ debugger_1.0/Debugger_test_1.0/src/main.cpp
@@ -7,13 +7,21 @@ int myFunction(int, int);
 void setup() {
   debug_init();
   pinMode(13,OUTPUT);
+  Serial3.begin(9600);//串口3不影响
+  Serial1.begin(9600);//串口1不影响
+  // Serial.begin(9600);//串口0影响
   // put your setup code here, to run once:
 }
 
 void loop() {
   digitalWrite(13,HIGH);
+  Serial3.println("sss!!!");
+   Serial1.println("sss!!!");
+  // Serial.println("aaa!!!");
   delay(1000);
   digitalWrite(13,LOW);
+  Serial3.println("sss###");
+  // Serial.println("aaa!!!");
   delay(1000);
   // put your main code here, to run repeatedly:
 }

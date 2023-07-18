858f75d7b0d4766e3996add429e30f48dbcfdb18
octopuslearn <3148262242@qq.com>
2023-07-18 09:41:00 +0800

    debugger有局限性，mega只能用RAM断点
    使得mills()返回相同值，无法使用

--- debugger_1.0/Debugger_test_1.0/src/main.cpp
+++ debugger_1.0/Debugger_test_1.0/src/main.cpp
@@ -2,26 +2,30 @@
 #include "avr8-stub.h"
 // #include "app_api.h"/*不需要这一行*/
 // put function declarations here:
-int myFunction(int, int);
+void clock_ms(unsigned int ms);
 
 void setup() {
   debug_init();
   pinMode(13,OUTPUT);
   Serial3.begin(9600);//串口3不影响
-  Serial1.begin(9600);//串口1不影响
+  // Serial1.begin(9600);//串口1不影响
   // Serial.begin(9600);//串口0影响
   // put your setup code here, to run once:
 }
 
 void loop() {
-  digitalWrite(13,HIGH);
-  Serial3.println("sss!!!");
-   Serial1.println("sss!!!");
-  // Serial.println("aaa!!!");
-  delay(1000);
-  digitalWrite(13,LOW);
+  Serial3.println("start!!!");
+  clock_ms(30);
   Serial3.println("sss###");
-  // Serial.println("aaa!!!");
-  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(500);
   // put your main code here, to run repeatedly:
 }
+
+void clock_ms(unsigned int ms)
+{
+  unsigned long last_time = millis();
+  digitalWrite(13,LOW);
+  while((millis()-last_time)<ms);
+}
+

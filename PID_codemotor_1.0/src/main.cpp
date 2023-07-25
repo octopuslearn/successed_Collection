af5a9a1407af6141d873dabb83e01c9f8cd30e49
octopuslearn <3148262242@qq.com>
2023-07-25 15:32:24 +0800

    速度环ok（有小问题，到目标后多跑了）

--- PID_codemotor_1.0/src/main.cpp
+++ PID_codemotor_1.0/src/main.cpp
@@ -1,118 +1,206 @@
 #include <Arduino.h>
+#include <PID_v1.h>
 
 /*以下，编码电机相关*/
-#define AIN1 3
-#define AIN2 4
-#define PWMA_LEFT 5//NC
-#define ENCODER_LEFT_A_PIN 2 //编码器
-int PPR = 11;              //一圈11个脉冲
-volatile long encoderCount=0;//存储电机脉冲数
-int rpm = 0; int setRPM=0;   //rpm: 实际测到的转速， setRPM:目标转速
+#define AIN1 22
+#define AIN2 23
+#define PWMA_LEFT  4//NC
+#define ENCODER_LEFT_A_1_PIN 2 //编码器
+#define ENCODER_LEFT_B_1_PIN 3 //编码器
+volatile int LEFT_A_posi=0;
+volatile int LEFT_B_posi=0;
 
+// #define AIN3 24
+// #define AIN4 25
+// #define PWMB_RIGHT 5
+// #define ENCODER_RIGHT_A_1_PIN 18 //编码器
+// #define ENCODER_RIGHT_B_1_PIN 19 //编码器
+// volatile int RIGHT_A_posi=0;
+// volatile int RIGHT_B_posi=0;
+ 
+int PPR = 11;                //一圈11个脉冲
+volatile long now_encoder=0; //当前编码器数值
+volatile long lastEncoded=0;//上一次编码器数值
+int  set_REV=0;              //设定点所需编码器数值
+int User_Input=120;            //设定角度
 /*以上，编码电机相关*/
 
-/*以下，电位器*/
-#define  pot A0//电位器
-/*以上，电位器*/
 
-unsigned int last_time;//上一次时间
+
+
+
 
 
 /*以下，PID相关*/
-float setPoint = 19;//滑轨中心与测距模块的距离
-
-float error;          //当前误差
-float previous_error; //上一次误差,用来计算D
-
-float kp=10;
-float ki=0.05;
-float kd=50;
-
-unsigned int dt=50;//每50ms进行一次计算
-
-float P;
-float I;
-float D;
-float PID;
+// double kp=1,ki=1,kd=0.05;//kp=5,ki=1,kd=0.01;
+// double kp=1,ki=1,kd=0.01;//kp=5,ki=1,kd=0.01;
+// double kp=1,ki=0.8,kd=0.01;//kp=5,ki=1,kd=0.01;
+// double kp=6,ki=0.8,kd=0.01;//kp=5,ki=1,kd=0.01;
+// double kp=1,ki=1,kd=0.01;//kp=5,ki=1,kd=0.01;
+double kp=0.4,ki=0,kd=0.001;//kp=5,ki=1,kd=0.01;//成功
+double input=0,output=0,setpoint=0;
+PID motor_A_PID(&input, &output, &setpoint, kp, ki, kd, DIRECT);
 /*以上，PID相关*/
 
 
 
+
+
+
+int lastMSB = 0;
+int lastLSB = 0;
+
+
+
+
+
+
+
+
+/*以下，范围*/
+int MIN_PWM=-125, MAX_PWM=125;
+unsigned long SAMPLE_TIME = 10;//采样时间
+/*以上，范围*/
+
+
 /*以下，函数声明*/
-void hc_sr04_read();
-void PID_contrl();
-void ISR_Encoder();
+void initPID();/*设定pid(pwm包括范围，模式等)*/
+void initMotors();/*初始化电机*/
+void initEncoders();/*初始化编码器*/
+void ISR_Encoder_LEFT_A_1_PIN();
+void pwmOut(int out);
+void forwardA(int pwm);
+void reverseA(int pwm);
+void brakeA();
 /*以上，函数声明*/
+
+
+
 
 
 
 
 void setup() {
   Serial.begin(9600);
-
-
-  /*以下，编码电机*/
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
-  pinMode(PWMA_LEFT, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_LEFT_A_PIN), ISR_Encoder, CHANGE);
-  /*以上，编码电机*/
-
-
-  last_time = millis();//获取上一次时间
-  // put your setup code here, to run once:
-
+  initMotors();/*初始化电机*/
+  initEncoders();/*初始化编码器*/
+  initPID();/*设定pid(pwm包括范围，模式等)*/
 }
 
 void loop() {
-  setRPM = map(analogRead(pot), 0, 1023, 2000, 10000);//设定转速
-  digitalWrite(AIN1, HIGH); digitalWrite(AIN2,LOW);   //电机方向
-  if(millis() > last_time+dt)
-  {
-    last_time = millis();//更新上一次时间
-    Serial.print("encoderCount: ");    Serial.print(encoderCount);
-    rpm = (float)((encoderCount/PPR)*(1000/dt)*60);//实际转速
-    Serial.print("起始Measured RPM: ");    Serial.print(rpm);   
-    PID_contrl();
-    analogWrite(PWMA_LEFT, PID); //调整马达转速
-
-        // 串口监视器上显示目标，Arduino 输出的 PWM， 以及测量到的RPM
-      Serial.print("Set RPM: ");         Serial.print(setRPM);
-      Serial.print('\t');
-      Serial.print("Measured RPM: ");    Serial.print(rpm);
-      Serial.print('\t');
-      Serial.print("PID: ");             Serial.println(PID);
-
-      //encoderCount = 0;// 累计脉冲数量重置,为下一秒测量做准备  
-  }
+// /*调试*/Serial.print("目标角度: "); Serial.print(User_Input); Serial.print("\t");
+// set_REV = map (User_Input, 0, 360, 0, 506); //将度数映射为脉冲
+set_REV=11*46*2*6;
+setpoint=set_REV;//PID工作时达到此值视为SET值
+/*调试*/Serial.print("目标角度对应脉冲数: "); Serial.print(setpoint); Serial.print("\t");
+input=now_encoder;//来自编码器的数据被视为过程值
+/*调试*/Serial.print("当前脉冲数: "); Serial.print(input); Serial.print("\t");
+motor_A_PID.Compute();//计算新的值输出
+/*调试*/Serial.print("output: "); Serial.print(output); Serial.println("\t");
+pwmOut(output);
+//  /*调试*/Serial.print("输出情况: "); Serial.println(output); 
+//  brakeA();
+// Serial.println("调整完成###");
 }
 
 
 
 
 
-void PID_contrl(){
-  error=rpm-setRPM;          //计算误差
-  P=kp*error;                       //P项
-  if(-4<error && error<4)
+
+
+
+/*设定pid(pwm包括范围，模式等)*/
+void initPID()
+{
+  motor_A_PID.SetMode(AUTOMATIC);//自动模式
+  motor_A_PID.SetSampleTime(SAMPLE_TIME);//设置采样时间-默认200ms 
+  motor_A_PID.SetOutputLimits(MIN_PWM, MAX_PWM);//设定PWM范围
+}
+
+/*初始化电机*/
+void initMotors(){
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMA_LEFT, OUTPUT);
+  
+
+  // pinMode(AIN3, OUTPUT);
+  // pinMode(AIN4, OUTPUT);
+  // pinMode(PWMB_RIGHT, OUTPUT);
+}
+
+
+/*初始化编码器*/
+void initEncoders(){
+  pinMode(ENCODER_LEFT_A_1_PIN,INPUT);
+  pinMode(ENCODER_LEFT_B_1_PIN,INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_LEFT_A_1_PIN), ISR_Encoder_LEFT_A_1_PIN, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_LEFT_A_1_PIN), ISR_Encoder_LEFT_A_1_PIN, CHANGE);
+  
+  
+  // pinMode(ENCODER_RIGHT_A_1_PIN,INPUT);
+  // pinMode(ENCODER_RIGHT_A_1_PIN,INPUT);
+  // attachInterrupt(digitalPinToInterrupt(ENCODER_RIGHT_A_1_PIN), ISR_Encoder_RIGHT_A_1_PIN, RISING);
+  // attachInterrupt(digitalPinToInterrupt(ENCODER_RIGHT_A_1_PIN), ISR_Encoder_RIGHT_B_1_PIN, RISING);
+}
+
+
+void ISR_Encoder_LEFT_A_1_PIN(){
+  if(digitalRead(ENCODER_LEFT_A_1_PIN)==HIGH)
   {
-    I += ki*error;                  //I项
+    if(digitalRead(ENCODER_LEFT_B_1_PIN)==LOW)
+      now_encoder++;
+    else
+      now_encoder--;
+  }
+  else 
+  {
+    if(digitalRead(ENCODER_LEFT_B_1_PIN)==HIGH)
+      now_encoder++;
+    else
+      now_encoder--;
+  }
+//  int MSB = digitalRead(ENCODER_LEFT_A_1_PIN); //MSB = most significant bit
+//  int LSB = digitalRead(ENCODER_LEFT_B_1_PIN); //LSB = least significant bit
+//  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+//  int sum = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+
+//  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) now_encoder ++;
+//  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) now_encoder --;
+
+//  lastEncoded = encoded; //store this value for next time
+}
+
+
+void pwmOut(int out)
+{
+  if(out>0)
+  {
+    forwardA(out);
   }
   else
   {
-    I = 0;
+    reverseA(abs(out));
   }
-  D = kd*((error-previous_error)/dt);//D项
+}
 
-  PID=P+I+D;                         //PID
-
-  if(PID>200) {PID=200;}             //限幅
-  if(PID<-200){PID=-200;}
-
-  previous_error = error;//更新上一刻误差
+void forwardA(int pwm){
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  analogWrite(PWMA_LEFT, pwm);
+  Serial.print("pwm: ");Serial.print(pwm);
 }
 
 
-void ISR_Encoder(){
-  encoderCount++;
+ void reverseA(int pwm){
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  analogWrite(PWMA_LEFT, pwm);
+} 
+
+
+void brakeA(){
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, LOW);
 }

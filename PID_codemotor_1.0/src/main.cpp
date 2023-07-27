d57fcc9f243c5f7339ec230d0455d80849e1a5c1
octopuslearn <3148262242@qq.com>
2023-07-27 20:44:44 +0800

    速度环搞定，问题是定时器2和4,5脚PWM,另外l298n电压只有6v（12v输入),故用tb6612,满负荷
    速度环搞定，问题是定时器2和4,5脚PWM,另外l298n电压只有6v（12v输入),故用tb6612,满负荷
    
    46*11*4

--- PID_codemotor_1.0/src/main.cpp
+++ PID_codemotor_1.0/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <PID_v1.h>
-
+#include <FlexiTimer2.h>
+// #include <TimerOne.h>
 /*以下，编码电机相关*/
 #define AIN1 22
 #define AIN2 23
@@ -20,9 +21,7 @@ volatile int LEFT_B_posi=0;
  
 int PPR = 11;                //一圈11个脉冲
 volatile long now_encoder=0; //当前编码器数值
-volatile long lastEncoded=0;//上一次编码器数值
-int  set_REV=0;              //设定点所需编码器数值
-int User_Input=120;            //设定角度
+int set_REV=0;              //设定点所需编码器数值
 /*以上，编码电机相关*/
 
 
@@ -37,7 +36,7 @@ int User_Input=120;            //设定角度
 // double kp=1,ki=0.8,kd=0.01;//kp=5,ki=1,kd=0.01;
 // double kp=6,ki=0.8,kd=0.01;//kp=5,ki=1,kd=0.01;
  //double kp=3*0.6,ki=0.1,kd=0.01;//kp=5,ki=1,kd=0.01;
-  double kp=1.6*0.6,ki=0.03,kd=0.01;//kp=5,ki=1,kd=0.01;
+double kp=1.6*0.6,ki=0.03,kd=0.01;//kp=5,ki=1,kd=0.01;
 // double kp=0.4,ki=0,kd=0.001;//kp=5,ki=1,kd=0.01;//成功
 double input=0,output=0,setpoint=0;
 PID motor_A_PID(&input, &output, &setpoint, kp, ki, kd, DIRECT);
@@ -48,9 +47,18 @@ PID motor_A_PID(&input, &output, &setpoint, kp, ki, kd, DIRECT);
 
 
 
-int lastMSB = 0;
-int lastLSB = 0;
 
+
+
+
+
+/*以下，速度环*/
+int set_REV_speed=0;
+double kp_speed=1.12,ki_speed=0,kd_speed=0;//kp=5,ki=1,kd=0.01;
+double input_speed=0,output_speed=0,setpoint_speed=0;
+float speed_rmp_in;//当前速度-圈/分钟
+PID motor_A_PID_speed(&input_speed, &output_speed, &setpoint_speed, kp_speed, ki_speed, kd_speed, DIRECT);
+/*以上，速度环*/
 
 
 
@@ -68,11 +76,14 @@ unsigned long SAMPLE_TIME = 10;//采样时间
 void initPID();/*设定pid(pwm包括范围，模式等)*/
 void initMotors();/*初始化电机*/
 void initEncoders();/*初始化编码器*/
-void ISR_Encoder_LEFT_A_1_PIN();
-void pwmOut(int out);
-void forwardA(int pwm);
-void reverseA(int pwm);
-void brakeA();
+void ISR_Encoder_LEFT_A_1_PIN();/*读取编码器脉冲*/
+void pwmOut(int out);/*pwm输出*/
+void forwardA(int pwm);/*前进*/
+void reverseA(int pwm);/*后退*/
+void brakeA();/*后退*/
+void PID_encoder_speed();/*编码器脉冲-速度环*/
+void PID_control_speed();/*速度环*/
+void PID_control_location();/*位置环*/
 /*以上，函数声明*/
 
 
@@ -83,30 +94,55 @@ void brakeA();
 
 void setup() {
   Serial.begin(9600);
+  
+
+  // Timer1.initialize(500000);
+  // Timer1.attachInterrupt(PID_encoder_speed); // blinkLED to run every 0.15 seconds
+  FlexiTimer2::set(10, 1.0/1000, PID_encoder_speed); // call every 500 1ms "ticks"
+  FlexiTimer2::start();
+  motor_A_PID_speed.SetMode(AUTOMATIC);//自动模式
+  motor_A_PID_speed.SetSampleTime(SAMPLE_TIME);//设置采样时间-默认200ms 
+  motor_A_PID_speed.SetOutputLimits(-255, 255);//设定PWM范围
+
+
+
   initMotors();/*初始化电机*/
   initEncoders();/*初始化编码器*/
-  initPID();/*设定pid(pwm包括范围，模式等)*/
+  // initPID();/*设定pid(pwm包括范围，模式等)*/
 }
 
 void loop() {
-// /*调试*/Serial.print("目标角度: "); Serial.print(User_Input); Serial.print("\t");
-// set_REV = map (User_Input, 0, 360, 0, 506); //将度数映射为脉冲
-set_REV=11*46*2*2;
-setpoint=set_REV;//PID工作时达到此值视为SET值
-/*调试*/Serial.print("目标角度对应脉冲数: "); Serial.print(setpoint); Serial.print("\t");
-input=now_encoder;//来自编码器的数据被视为过程值
-motor_A_PID.Compute();//计算新的值输出
-/*调试*/Serial.print("被调值: "); Serial.print(input); Serial.print("\t");
-pwmOut(output);
-/*调试*/Serial.print("输出情况: "); Serial.println(output); 
-//  brakeA();
-// Serial.println("调整完成###");
+  PID_control_speed();
 }
 
 
 
+/*速度环*/
+void PID_control_speed()
+{
+  set_REV_speed=100;
+  /*调试*/Serial.print("目标速度-rpm/min: "); Serial.print(set_REV_speed); Serial.print("\t");
+  setpoint_speed=set_REV_speed;//PID工作时达到此值视为SET值
+  input_speed=speed_rmp_in;//被调值
+  /*调试*/Serial.print("#####-被调值: "); Serial.print(input_speed); Serial.print("\t");
+  motor_A_PID_speed.Compute();
+  /*调试*/Serial.print("当前速度-被调值: "); Serial.print(input_speed); Serial.print("\t");
+  pwmOut(output_speed);
+  /*调试*/Serial.print("速度-输出情况: "); Serial.println(output_speed);
+}
 
-
+/*位置环*/
+void PID_control_location()
+{
+  set_REV=11*46*2*2;
+  setpoint=set_REV;//PID工作时达到此值视为SET值
+  /*调试*/Serial.print("目标角度对应脉冲数: "); Serial.print(setpoint); Serial.print("\t");
+  input=now_encoder;//来自编码器的数据被视为过程值
+  motor_A_PID.Compute();//计算新的值输出
+  /*调试*/Serial.print("被调值: "); Serial.print(now_encoder); Serial.print("\t");
+  pwmOut(255);
+  /*调试*/Serial.print("输出情况: "); Serial.println(output); 
+}
 
 
 
@@ -145,7 +181,7 @@ void initEncoders(){
   // attachInterrupt(digitalPinToInterrupt(ENCODER_RIGHT_A_1_PIN), ISR_Encoder_RIGHT_B_1_PIN, RISING);
 }
 
-
+/*读取编码器脉冲*/
 void ISR_Encoder_LEFT_A_1_PIN(){
   if(digitalRead(ENCODER_LEFT_A_1_PIN)==HIGH)
   {
@@ -172,7 +208,7 @@ void ISR_Encoder_LEFT_A_1_PIN(){
 //  lastEncoded = encoded; //store this value for next time
 }
 
-
+/*pwm输出*/
 void pwmOut(int out)
 {
   if(out>0)
@@ -185,21 +221,29 @@ void pwmOut(int out)
   }
 }
 
+/*前进*/
 void forwardA(int pwm){
   digitalWrite(AIN1, LOW);
   digitalWrite(AIN2, HIGH);
   analogWrite(PWMA_LEFT, pwm);
 }
 
-
+/*后退*/
  void reverseA(int pwm){
   digitalWrite(AIN1, HIGH);
   digitalWrite(AIN2, LOW);
   analogWrite(PWMA_LEFT, pwm);
 } 
 
-
+/*停车*/
 void brakeA(){
   digitalWrite(AIN1, LOW);
   digitalWrite(AIN2, LOW);
+}
+
+/*编码器脉冲-速度环*/
+void PID_encoder_speed()
+{
+  speed_rmp_in=(6000*now_encoder)/2024.0;//(60*20*电机脉冲)/46*11*4
+  now_encoder=0;
 }

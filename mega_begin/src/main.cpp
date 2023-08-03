f2ff357d07ccf2400214aad13c073c1b58b042d0
octopuslearn <3148262242@qq.com>
2023-08-03 21:49:40 +0800

    第一个任务完成，记住不要把自动回正写到loop里
    void loop() {
      do
      {
        /* code */
        myservo_x.writeMicroseconds(start_x);
        delay(15);
        myservo_y.writeMicroseconds(start_y);
        delay(15);
      } while (0);

--- mega_begin/src/main.cpp
+++ mega_begin/src/main.cpp
@@ -44,15 +44,6 @@ void OLED_reportStatus();/*OELD获取当前舵机信息*/
 /*以上，函数声明*/
 
 
-/*以下，新添*/
-void contal_pulse(char servoName,int toPos,int servoDelay);/*直接到位*/
-void contrl_reading(char serialCmd,int servoDelay); /*一点一点动*/
-void writeMicroseconds_button_fine_tuning();        /*以下，法2，按键移动调试*/
-int contrl_reading_baseJoyPos=500;
-int contrl_reading_moveStep=1;
-/*以上，新添*/
-
-
 /*以下，获取当前脉冲*/
 void get_angle();/*获取当前脉冲*/
 int get_angle_x;//当前脉冲
@@ -60,10 +51,25 @@ int get_angle_y;
 /*以上，获取当前脉冲*/
 
 
+int start_x=1410;
+int start_y=1330;
+
+/*以下，新添*/
+void contal_pulse(char servoName,int toPos,int servoDelay);/*直接到位*/
+void contrl_reading(char serialCmd,int servoDelay); /*一点一点动*/
+void writeMicroseconds_button_fine_tuning();        /*以下，法2，按键移动调试*/
+int contrl_reading_baseJoyPos_x=start_x;
+int contrl_reading_baseJoyPos_y=start_y;
+int contrl_reading_moveStep=3;
+/*以上，新添*/
+
+
+
+
 
 
 void setup() {
-  Serial.begin(9600);
+  // Serial.begin(9600);
   
   u8g2.begin();              // 初始化演示器
   u8g2.setColorIndex(1);
@@ -80,10 +86,15 @@ void setup() {
   myservo_x.attach(myservo_x_pin,500,2500);//舵机连接位置/*此处更改了默认的180-2400*/
   myservo_y.attach(myservo_y_pin,500,2500);
 
-  // myservo_x.write(0);//舵机起始位置
-  // delay(10);
-  // myservo_y.write(0); 
-  // delay(10);
+  myservo_x.write(0);//舵机起始位置
+  delay(10);
+  myservo_y.write(0); 
+  delay(10);
+  
+  // myservo_x.writeMicroseconds(start_x);
+  // delay(15);
+  // myservo_y.writeMicroseconds(start_y);
+  // delay(15);
 
   int myservo_x_star=myservo_x.read();
   int myservo_y_star=myservo_y.read();
@@ -106,6 +117,16 @@ void setup() {
 
 
 void loop() {
+  do
+  {
+    /* code */
+    myservo_x.writeMicroseconds(start_x);
+    delay(15);
+    myservo_y.writeMicroseconds(start_y);
+    delay(15);
+  } while (0);
+  
+
   // armDataCmd('x', 87, DSD);
   // armDataCmd('y', 90, DSD);
   // while(digitalRead(test_now) != LOW);
@@ -127,7 +148,7 @@ void loop() {
 
   // get_angle();//获取当前脉冲
   // contal_pulse('x',1000,DSD);/*直接到位*/
-  // writeMicroseconds_button_fine_tuning();/*以下，法2，按键移动调试*/
+   writeMicroseconds_button_fine_tuning();/*以下，法2，按键移动调试*/
 }
 
 
@@ -400,12 +421,12 @@ void contrl_reading(char serialCmd,int servoDelay)
    switch(serialCmd){
     case 'a':  // x_Base向左
         Serial.println("x_Base向左");                
-      contrl_reading_baseJoyPos += contrl_reading_moveStep;
-            if(contrl_reading_baseJoyPos>2500)  contrl_reading_baseJoyPos=2500;
-            if(contrl_reading_baseJoyPos<500)  contrl_reading_baseJoyPos=500;
-      myservo_x.writeMicroseconds(contrl_reading_baseJoyPos);
+      contrl_reading_baseJoyPos_x += contrl_reading_moveStep;
+            if(contrl_reading_baseJoyPos_x>2500)  contrl_reading_baseJoyPos_x=2500;
+            if(contrl_reading_baseJoyPos_x<500)  contrl_reading_baseJoyPos_x=500;
+      myservo_x.writeMicroseconds(contrl_reading_baseJoyPos_x);
       delay(servoDelay);
-            Serial.print("x_Base向左: ");Serial.println(contrl_reading_baseJoyPos); 
+            Serial.print("x_Base向左: ");Serial.println(contrl_reading_baseJoyPos_x); 
             Serial.println("x_Base向左###");
 /*以下，显示*/
         u8g2.firstPage();
@@ -415,19 +436,19 @@ void contrl_reading(char serialCmd,int servoDelay)
           u8g2.drawStr(0, 10, "contrl_reading");
           u8g2.drawStr(0,25, "cr_x");
           u8g2.setCursor(sizeof("cr_x: ") * 6, 25);
-          u8g2.print(contrl_reading_baseJoyPos);
+          u8g2.print(contrl_reading_baseJoyPos_x);
         } while (u8g2.nextPage());
 /*以上，显示*/
       break;  
       
     case 'b':  // x_Base向右
         Serial.println("x_Base向右");                
-      contrl_reading_baseJoyPos -= contrl_reading_moveStep;
-            if(contrl_reading_baseJoyPos>2500)  contrl_reading_baseJoyPos=2500;
-            if(contrl_reading_baseJoyPos<500)  contrl_reading_baseJoyPos=500;
-      myservo_x.writeMicroseconds(contrl_reading_baseJoyPos);
+      contrl_reading_baseJoyPos_x -= contrl_reading_moveStep;
+            if(contrl_reading_baseJoyPos_x>2500)  contrl_reading_baseJoyPos_x=2500;
+            if(contrl_reading_baseJoyPos_x<500)  contrl_reading_baseJoyPos_x=500;
+      myservo_x.writeMicroseconds(contrl_reading_baseJoyPos_x);
       delay(servoDelay);
-            Serial.print("x_Base向右: ");Serial.println(contrl_reading_baseJoyPos); 
+            Serial.print("x_Base向右: ");Serial.println(contrl_reading_baseJoyPos_x); 
             Serial.println("x_Base向右###"); 
 /*以下，显示*/
         u8g2.firstPage();
@@ -437,19 +458,19 @@ void contrl_reading(char serialCmd,int servoDelay)
           u8g2.drawStr(0, 10, "contrl_reading");
           u8g2.drawStr(0,25, "cr_x");
           u8g2.setCursor(sizeof("cr_x: ") * 6, 25);
-          u8g2.print(contrl_reading_baseJoyPos);
+          u8g2.print(contrl_reading_baseJoyPos_x);
         } while (u8g2.nextPage());
 /*以上，显示*/
       break;        
  
     case 's':  // y_rArm向下
         Serial.println("y_rArm向下");                
-      contrl_reading_baseJoyPos += contrl_reading_moveStep;
-            if(contrl_reading_baseJoyPos>2500)  contrl_reading_baseJoyPos=2500;
-            if(contrl_reading_baseJoyPos<500)  contrl_reading_baseJoyPos=500;
-      myservo_y.writeMicroseconds(contrl_reading_baseJoyPos);
+      contrl_reading_baseJoyPos_y += contrl_reading_moveStep;
+            if(contrl_reading_baseJoyPos_y>2500)  contrl_reading_baseJoyPos_y=2500;
+            if(contrl_reading_baseJoyPos_y<500)  contrl_reading_baseJoyPos_y=500;
+      myservo_y.writeMicroseconds(contrl_reading_baseJoyPos_y);
       delay(servoDelay);
-            Serial.print("y_rArm向下: ");Serial.println(contrl_reading_baseJoyPos);
+            Serial.print("y_rArm向下: ");Serial.println(contrl_reading_baseJoyPos_y);
             Serial.println("y_rArm向下###");
 /*以下，显示*/
         u8g2.firstPage();
@@ -459,19 +480,19 @@ void contrl_reading(char serialCmd,int servoDelay)
           u8g2.drawStr(0, 10, "contrl_reading");
           u8g2.drawStr(0,40, "cr_y");
           u8g2.setCursor(sizeof("cr_y: ") * 6, 40);
-          u8g2.print(contrl_reading_baseJoyPos);
+          u8g2.print(contrl_reading_baseJoyPos_y);
         } while (u8g2.nextPage());
 /*以上，显示*/
       break;  
                  
     case 'w':  // y_rArm向上
         Serial.println("y_rArm向上");     
-      contrl_reading_baseJoyPos -= contrl_reading_moveStep;
-            if(contrl_reading_baseJoyPos>2500)  contrl_reading_baseJoyPos=2500;
-            if(contrl_reading_baseJoyPos<500)  contrl_reading_baseJoyPos=500;
-      myservo_y.writeMicroseconds(contrl_reading_baseJoyPos);
+      contrl_reading_baseJoyPos_y -= contrl_reading_moveStep;
+            if(contrl_reading_baseJoyPos_y>2500)  contrl_reading_baseJoyPos_y=2500;
+            if(contrl_reading_baseJoyPos_y<500)  contrl_reading_baseJoyPos_y=500;
+      myservo_y.writeMicroseconds(contrl_reading_baseJoyPos_y);
       delay(servoDelay);
-            Serial.print("y_rArm向上: ");Serial.println(contrl_reading_baseJoyPos);
+            Serial.print("y_rArm向上: ");Serial.println(contrl_reading_baseJoyPos_y);
             Serial.println("y_rArm向上###"); 
 /*以下，显示*/
         u8g2.firstPage();
@@ -481,7 +502,7 @@ void contrl_reading(char serialCmd,int servoDelay)
           u8g2.drawStr(0, 10, "contrl_reading");
           u8g2.drawStr(0,40, "cr_y");
           u8g2.setCursor(sizeof("cr_y: ") * 6, 40);
-          u8g2.print(contrl_reading_baseJoyPos);
+          u8g2.print(contrl_reading_baseJoyPos_y);
         } while (u8g2.nextPage());
 /*以上，显示*/
       break;  
